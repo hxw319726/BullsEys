469dbb1b18bca07efcbca06961244036a77aa113
黄晓伟 <1140480831@qq.com>
2018-11-29 10:17:38 +0800

    排布局，添加读取滑块函数和通过showAlert显示滑块数值

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue:Int=50
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,11 +22,23 @@ class ViewController: UIViewController {
     }
 
 
+    /// 显示滑块数值
     @IBAction func showAlert(){
-        let alert=UIAlertController(title: "Hello Russia", message: "梅西上演帽子戏法拯救阿根廷", preferredStyle: .alert)
-        let action = UIAlertAction(title: "2018年不再无聊", style: .default, handler: nil)
+        let message="滑动条的当前数值是：\(currentValue)"
+        let alert=UIAlertController(title: "Hello Messi", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert,animated: true,completion: nil)
+    }
+    
+    
+    
+    /// 读取滑块的数值
+    ///
+    /// - Parameter slider: 滑块数值
+    @IBAction func slideMoved(slider:UISlider){
+//         print("滑动条的当前数值是： \(slider.value)")
+        currentValue=lroundf(slider.value)
     }
 }
 
